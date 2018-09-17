b38e1a4608519b1c856e6dcb760a1382c61b547c
Nabil5352 <report.nabil@gmail.com>
2018-09-18 00:08:42 +0600

    vuejs boilerplate

--- src/test/App.spec.js
+++ src/test/App.spec.js
@@ -1,7 +1,7 @@
 import { mount, createLocalVue } from '@vue/test-utils'
 import App from '../App'
 
-test('App has a .center-content class', () => {
+test('App has h1 tag', () => {
 	const vue = createLocalVue()
 	const app = mount (App, {vue})
 
